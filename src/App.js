4226b4185b689b01eef43069d769d6cd45d5526d
mpsolares <marianasolares@gmail.com>
2022-08-20 21:30:12 -0300

    first commit add files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let img = "hamburger.jpg"
+  let title = "Grand Triple McBacon";
+  let cal = "0 kcal";
+  let description = "¡NUEVO PAN! Triple carne, inigualable queso cheddar, cebolla, kétchup y mostaza, y el increíble ingrediente que lo hace único: bacon premium";
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div>"
+    <div className="container">
+      <div className="row">
+        <div className="col-md-4 offset-md-2">
+          <img src={img} alt={title} className="img-fluid"/>
+        </div>
+        <div className="col-md-4">
+          <h1>{title}</h1>
+          <p>{cal}</p>
+          <p>{description}</p>
+        </div>
+      </div>
     </div>
+
+   </div>
   );
 }
 
